4c6c767e7bfde398d5edc743888f7c5c1dcf07c8
NakhimoVV <tj1993@bk.ru>
2024-11-26 00:06:30 +0300

    try: add chart #3

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -9,6 +9,7 @@ interface ChartProps {
 
 const Chart: React.FC<ChartProps> = ({ data }) => {
     const barWidth = 80
+    const spacing = 60
     const maxValue = 500
 
     return (
@@ -27,38 +28,38 @@ const Chart: React.FC<ChartProps> = ({ data }) => {
                     <line x1="0" y1="0" x2="500" y2="0" stroke="red" />
                     <line x1="0" y1="0" x2="0" y2={maxValue} stroke="red" />
                     {/* перечисление Bar'ов */}
-                    {Object.entries(data).map(([key, value]) => {
-                        if (key === 'dev') {
-                            return (
-                                <g key={key}>
-                                    {/* перечисление BarItem'ов */}
-                                    {Object.entries(value).map(([k, kData]) => {
-                                        return (
-                                            <BarItem
-                                                key={k}
-                                                nameComponent={
-                                                    k as 'front' | 'back' | 'db'
-                                                }
-                                                data={value}
-                                            />
-                                        )
-                                    })}
-                                    <text
-                                        x={barWidth / 2}
-                                        y={20}
-                                        fill="black"
-                                        textAnchor="middle"
-                                        fontSize="12"
-                                        transform="scale(1, -1)"
-                                    >
-                                        {key}
-                                    </text>
-                                </g>
-                            )
-                        }
-                        return null
-                        // if (key === 'test') {}
-                        // if (key === 'prod') {}
+                    {Object.entries(data).map(([key, value], index) => {
+                        if (key === 'title' || key === 'norm') return null
+                        const translateX = (index - 1) * (barWidth + spacing)
+                        return (
+                            <g
+                                key={key}
+                                transform={`translate(${translateX},0)`}
+                            >
+                                {/* перечисление BarItem'ов */}
+                                {Object.entries(value).map(([k, kData]) => {
+                                    return (
+                                        <BarItem
+                                            key={k}
+                                            nameComponent={
+                                                k as 'front' | 'back' | 'db'
+                                            }
+                                            data={value}
+                                        />
+                                    )
+                                })}
+                                <text
+                                    x={barWidth / 2}
+                                    y={20}
+                                    fill="black"
+                                    textAnchor="middle"
+                                    fontSize="12"
+                                    transform="scale(1, -1)"
+                                >
+                                    {key}
+                                </text>
+                            </g>
+                        )
                     })}
                 </g>
             </svg>
